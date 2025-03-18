73834c35d10581358a920840525bec0ba72051a8
Joel Justino <134508163+justinojjsj@users.noreply.github.com>
2025-03-18 14:50:39 -0300

    Delete _scripts directory

--- _scripts/push_db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /home/silvajunior/desenvolvimento/pi5-tempguardian/
-
-git checkout db_e_log-atualizados
-git add --all
-git commit -a -m 'Atualização do banco de dados e dos logs'
-git push
